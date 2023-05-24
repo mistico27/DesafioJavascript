b98495e4e370c214f861a3a036b97a9fc05225ee
chris_beltran <cristianb72@hotmail.com>
2023-05-23 20:50:10 -0600

    mama

--- js/details.js
+++ js/details.js
@@ -16,14 +16,14 @@ const printCard = async() => {
     let CardInfo = await getCardInfo(postCardId)
     let html = `<div class="card mb-3" style="max-width: 540px;">
     <div class="row g-0">
-      <div class="col-md-4">
+      <div class="col-md-8">
         <img src=${CardInfo.picture} class="img-fluid rounded-start" style="height:100%" alt="...">
       </div>
-      <div class="col-md-8">
+      <div class="col-md-4">
         <div class="card-body">
           <h5 class="card-title">Autor:${CardInfo.autor} </h5>
           <h3 class="card-title">Titulo:${CardInfo.titulo} </h3>
-          <p class="card-text">Origin: ${CardInfo.postBody}</p>
+          <p class="card-text">Body: ${CardInfo.postBody}</p>
           <button class="btn btn-primary" id="backButton">Regresar</button>
         </div>
       </div>
