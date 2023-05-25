24cca892bb3165fcfd1977a977cadcb8ec61e6e4
daniel.flores <dani.fguerrero@gmail.com>
2023-05-25 02:04:37 -0600

    Update Login - con localStorage

--- js/session.js
+++ js/session.js
@@ -1,8 +1,28 @@
-// let email="";
-// let pass=""
+let ext=null;
+if (localStorage.getItem('isLoggedIn') === 'true') {
+    // window.open("Index.html","_self")
+   
 
-let statusSesion = localStorage.getItem(email,pass);
-// console.log(statusSesion);
-// statusSesion ? window.open("Index.html","_self") : window.open("sesion/index2.html","_self")
-if (statusSesion === null) {window.open("sesion/index2.html","_self"); }
-else {window.open("Index.html","_self")} 
+    // // console.log("Estoy logueado")
+    ext='si';
+} else { 
+    window.location.replace("sesion/index2.html"); 
+     ext='no';
+}
+
+switch (ext){
+    case 'si': 
+        window.open("Index.html")
+        console.log("Estoy logueado")
+    break;
+    case 'no': 
+    window.location.replace("sesion/index2.html");
+    break;
+    default:
+    window.location.replace("login.html");
+
+ }
+
+let signOut = document.getElementById("singOut");
+signOut.addEventListener("click",e =>{let clean = localStorage.clear();
+    window.location.replace("/login.html"); }) 
