d5645d7bc1d32314819121c58133863a425fa282
chris_beltran <cristianb72@hotmail.com>
2023-05-25 00:41:29 -0600

    login

--- js/login.js
+++ js/login.js
@@ -1,5 +1,4 @@
-const BASE_URL ='https://retojs-g25-default-rtdb.firebaseio.com/';
-
+const BASE_URL ='https://retojs-g25-default-rtdb.firebaseio.com/users/';
 let statusSesion = localStorage.getItem("user");
 console.log(statusSesion);
 statusSesion ? window.open("Index.html","_self") : null
@@ -25,53 +24,47 @@ function showAlert(message,className){
     setTimeout(()=>document.querySelector('.alert').remove(),2500)
   
   }
-
-let email = document.getElementById("InputEmail");
     
 let pass = document.getElementById("InputPassword").value;
 const continueBotton =document.getElementById("continueButton");
-continueBotton.addEventListener('click',async (e)=>{
- let email = document.getElementById("InputEmail").value;
- let pass = document.getElementById("InputPassword").value;
+continueBotton.addEventListener("click", async (e) => {
+  let email = document.getElementById("InputEmail").value;
+  let pass = document.getElementById("InputPassword").value;
   e.preventDefault();
   let UserInfo = await getAllUsers();
-  console.log(UserInfo);
-  const newSortArray =Object.values(UserInfo);
+  const newSortArray = Object.values(UserInfo);
 
-  let passArray=[];
-  let emailArray=[];
-  let passstatus=false;
-  let emailStatus=false;
-  for(let i=0; i<newSortArray.length; i++){
+  let passArray = [];
+  let emailArray = [];
+  let passstatus = false;
+  let emailStatus = false;
+  for (let i = 0; i < newSortArray.length; i++) {
     passArray.push(newSortArray[i].pass);
     emailArray.push(newSortArray[i].email);
-}
-for(let z=0;z<passArray.length;z++){
-    if(pass==passArray[z]){
-        passstatus= true;
-    }else{
-       passstatus;
+  }
+  for (let z = 0; z < passArray.length; z++) {
+    if (pass == passArray[z]) {
+      passstatus = true;
+    } else {
+      passstatus;
     }
-}
+  }
 
-for(let z=0;z<emailArray.length;z++){
-    if(email==emailArray[z]){
-        emailStatus= true;
-    }else{
-        emailStatus;
+  for (let z = 0; z < emailArray.length; z++) {
+    if (email == emailArray[z]) {
+      emailStatus = true;
+    } else {
+      emailStatus;
     }
-}
+  }
 
-if(emailStatus&&passstatus){
-    let statusSesion = localStorage.setItem(email,pass);
-
-    window.location.replace(`./Index.html`);
-}else if(email === "" || pass === "" ){
-    showAlert('campos nulos , favor de verificar','danger');
-}else { 
-    showAlert('campos erroneos , favor de verificar','danger');
-
-
-}
-
+  if (emailStatus && passstatus) {
+    let statusSesion = localStorage.setItem(email, pass);
+    window.location.replace("Index.html");
+    
+  } else if (email === "" || pass === "") {
+    showAlert("campos nulos , favor de verificar", "danger");
+  } else {
+    showAlert("campos erroneos , favor de verificar", "danger");
+  }
 });
